0d9bbdf1c51eaf55bf70e73784fa287b65623400
Zeigfreid <various.authors@gmail.com>
2012-12-18 16:34:38 -0800

    Added options to backup and restore
    -n for making a store with a special name (like "unicorns")
    -d and -h are unimplemented
    
    The last thing I did was start testing restore with -n. I was going
    to watch the sessionstore.js file and compare (wow, I should write some
    tests)

--- lib/foxhole/commands.rb
+++ lib/foxhole/commands.rb
@@ -7,16 +7,15 @@ module Foxhole
 
   def Foxhole.backup(options={})
     logger.debug('in backup')
-    logger.debug('  options' + options.inspect)
-    logger.debug('  options' + options[:d])
-    
-    string_h = options[:d]
-    # look in the .foxhole and see if today's date is there
-    logger.debug('  hello: ' + string_h.nil?.to_s)
-    backup_dir(string_h)
-    backup_dir(options[:d]).mkpath unless backup_dir.exist?
-    logger.debug('  made dir')
+    logger.debug('options ' + options.inspect)
+    logger.debug('options[:n] ' + options[:n])
+    logger.debug('backup_dir' + backup_dir(options[:n]).to_path)
 
+    # TODO super weird: I can't run backup_dir(options[:n]).mkpath for some reason
+
+    # look in the .foxhole and see if today's date is there
+    backup_dir.mkpath unless backup_dir.exist?
+    logger.debug('  backing up to: ' + backup_dir.to_path)
 
     # copy the sessionstore.js into that directory along with
     # a timestamp sessionstore_828928928.js
@@ -28,15 +27,33 @@ module Foxhole
 
   # overwrite the current sessionstore.js with the most
   # recent backup (or a particular backup)
-  def Foxhole.restore
-    # find the most recent folder by sorting them and taking the last
-    recent_backup = Pathname.new(backup_dir.parent)
-    recent_backup = recent_backup + recent_backup.entries.sort.last
+  def Foxhole.restore(options={})
+    logger.debug('in restore')
+    if options[:n] && options[:d]
+      puts "Restoring by name conflicts with restoring by date\n \
+              run foxhole restore with either -n or -d but not both"
+    end
 
-    # then find the most recent backup by the same method
-    recent_backup = recent_backup + recent_backup.entries.sort.last
+    unless options[:n]
+      logger.debug('options :n nil')
+      # find the most recent folder by sorting them and taking the last
+      foxhole_dirs = Pathname.new(backup_dir.parent).entries
+      chronological_dirs = foxhole_dirs.select do |item| 
+        item.to_path =~ /.*\/\d{4}\.\d{2}\.\d{2}/
+      end
 
-    FileUtils.cp(recent_backup, sessionstore)
+      salient_backup_dir = chronological_dirs.sort.last
+    else
+      salient_backup_dir = backup_dir(options[:n])
+    end
+
+    puts "salient_backup_dir: " + salient_backup_dir.inspect
+
+    # then find the most recent backup or the backup matching the given time
+    salient_backup = Pathname.new(salient_backup_dir) + salient_backup_dir.entries.sort.last
+
+    FileUtils.cp(salient_backup, sessionstore)
+    logger.debug('in out')
   end
 
   def Foxhole.config
@@ -50,20 +67,21 @@ module Foxhole
       :backup_dir => "~/.foxhole" }
   end
 
-  def Foxhole.backup_dir(directory)
-    logger.debug('in backup_dir')
+  def Foxhole.backup_dir(directory=nil)
+    logger.debug('in backup_dir with ' + directory.to_s)
     @@backup_dir ||= begin
       if directory.nil?
-        logger.debug('dir was nil')
+        logger.debug('dir was nil, using date')
         datetime = DateTime::now
-        backup_date = "#{datetime.year}.#{datetime.month}.#{datetime.day}"
-        backup_dir = Pathname.new(File.expand_path(config[:backup_dir])) + backup_date
+        backup_date = datetime.strftime("%Y.%m.%d")
+
+        backup_dirpath = Pathname.new(File.expand_path(config[:backup_dir])) + backup_date
       else
-        backup_dir = Pathname.new(File.expand_path(config[:backup_dir])) + directory
-        logger.debug('backup: ' + backup_dir.to_path)
+        backup_dirpath = Pathname.new(File.expand_path(config[:backup_dir])) + directory
+        logger.debug('backup_dirpath: ' + backup_dirpath.to_path)
       end
 
-      backup_dir
+      backup_dirpath
     end
   end
 
